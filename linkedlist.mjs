bb695f8b46d4c8514f98502ae92015babaf9f9d5
João Vítor Ferraz <jvkferraz@gmail.com>
2024-04-29 12:22:10 -0300

    At method

--- linkedlist.mjs
+++ linkedlist.mjs
@@ -18,6 +18,30 @@ export default class LinkedList {
     tail.next = node;
   }
 
+  prepend(value) {
+    const newHead = new Node();
+    const oldHead = this.head;
+
+    newHead.value = value;
+    newHead.next = oldHead;
+    this.head = newHead;
+  }
+
+  at(index) {
+    let node = this.head;
+    let counter = 0;
+
+    while (node !== null) {
+      if (counter === index) {
+        return node;
+      }
+      node = node.next;
+      counter += 1;
+    }
+
+    return null;
+  }
+
   size() {
     let node = this.head;
     let size = 0;
